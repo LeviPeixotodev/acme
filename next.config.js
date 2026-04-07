36c53ee0cc4912f50715b1177d04b9d0f21227af
LeviPeixoto.dev <levipeixoto.dev@gmail.com>
2026-04-07 18:14:03 +0000

    chore: configurar next com modo estrito do react

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/**@type {import('next'.NextConfig)} */
+const nextConfig={
+  reactStrictMode:true
+};
+export default nextConfig
+/*para que se tiver alguma funçao obsoleta ele avisa no terminal e erros em geral*/
